fb224672bbdb5427d7d01bb36f9bdcb8ac43c378
will van wart <william@wvwproductions.com>
2016-11-04 16:28:42 -0400

    new survey spec

---
+++ spec/features/new_survey_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'taking a new survey' do
+
+  # this is currently not passing
+  it 'successfully submits survey with answer after button click' do
+    survey = Survey.create({title: "Test", questions_attributes: [{title: "question"}]})
+    visit surveys_path
+    click_link 'take survey'
+    expect(current_path).to eq(survey_path(survey))
+    fill_in :answers_url[1], with: "new answer"  #answers_path
+    click_button 'Submit Survey'
+    expect {click_button "Submit Survey"}.to change(answer, :count)
+  end
+end
