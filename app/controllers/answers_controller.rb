0c2ff3c021fccd54b62774e66d3f2939e76d71e6
will van wart <william@wvwproductions.com>
2016-11-04 15:48:56 -0400

    build answer_controller

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,12 @@
+class AnswersController < ApplicationController
+  def create
+    survey = Survey.find(params[:survey_id])
+    if survey
+      params[:answers].each_pair do |question_id, answer|
+        question = survey.questions.find(question_id)
+        question.answers.create({body: answer, user: current_user}) if question
+      end
+    end
+    redirect_to survey_path(survey)
+  end
+end
