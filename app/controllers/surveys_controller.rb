316749864fd2ceb37df0fb92ee63932fe577636f
will van wart <william@wvwproductions.com>
2016-11-04 14:04:02 -0400

    build surveys_controller

---
+++ app/controllers/surveys_controller.rb
@@ -0,0 +1,47 @@
+class SurveysController < ApplicationController
+
+  def index
+    @surveys = Survey.all
+  end
+
+  def show
+    @survey = Survey.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @survey = @user.surveys.find(params[:id])
+    if @survey.update(survey_params)
+      redirect_to @survey, notice: "Edit successful"
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @survey = Survey.new
+    @survey.questions.build
+  end
+
+  def create
+    @survey = current_user.surveys.build(survey_params)
+    if @survey.save
+      redirect_to @survey, notice: "Successfully created"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @survey = Survey.find(params[:id]).destroy
+    redirect_to surveys_path, notice: "successfully deleted"
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey).permit(:title, questions_attributes: [:id, :title])
+  end
+end
